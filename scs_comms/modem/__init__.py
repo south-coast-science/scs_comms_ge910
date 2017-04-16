e10142d2bfe9cf33eeb04bcb175b66aea08ceff8
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-04-16 12:10:32 +0100

    Moved class init() invocations to __init__.py

---
+++ scs_comms/modem/__init__.py
@@ -0,0 +1,12 @@
+"""
+Created on 16 Apr 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_comms.modem.pca8574 import PCA8574State
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+PCA8574State.init()
